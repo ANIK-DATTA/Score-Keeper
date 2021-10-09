f238886c33516a0eb94ff6b51fc9d9207ace606c
shaurya5 <gargshaurya28@gmail.com>
2021-10-09 19:07:40 +0530

    Updated the reset function in app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const p1button = document.querySelector('#p1button');
 const p2button = document.querySelector('#p2button');
 const setbtn = document.querySelector('#setBtn');
+const scoreField = document.querySelector('#winningScore');
 
 let p1score = 0, p2score = 0;
 let winningScore;
 
 setbtn.addEventListener('click', function(){
-    winningScore = parseInt(document.querySelector('#winningScore').value);
+    winningScore = parseInt(scoreField.value);
     console.log(winningScore);
 })
 
@@ -14,8 +15,8 @@ p1button.addEventListener('click', function (){
     p1score++; 
     document.querySelector("#p1display").innerHTML = p1score;
     if(p1score === winningScore){
-        document.querySelector("#p1button").disabled = true;
-        document.querySelector("#p2button").disabled = true;
+        p1button.disabled = true;
+        p2button.disabled = true;
         document.querySelector('#winner').innerHTML = "Player 1 Wins";
     }
 })
@@ -24,18 +25,21 @@ p2button.addEventListener('click', function (){
     p2score++;
     document.querySelector("#p2display").innerHTML = p2score;
     if(p2score === winningScore){
-        document.querySelector("#p1button").disabled = true;
-        document.querySelector("#p2button").disabled = true;
+        p1button.disabled = true;
+        p2button.disabled = true;
         document.querySelector('#winner').innerHTML = "Player 2 Wins";
     }
 })
 
 
 reset.addEventListener('click', function (){
+    p1score = 0;
+    p2score = 0;
     document.querySelector("#p2display").innerHTML = 0;
     document.querySelector("#p1display").innerHTML = 0;
-    document.querySelector("#p1button").disabled = false;
-    document.querySelector("#p2button").disabled = false;
+    p1button.disabled = false;
+    p2button.disabled = false;
+    scoreField.value = null;
     document.querySelector('#winner').innerHTML = "";
 })
 
