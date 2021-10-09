42f7ec0d2a3e8d8c60cef0ce7c46c0c38be1f076
shaurya5 <gargshaurya28@gmail.com>
2021-10-09 22:28:24 +0530

    changed app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const p1button = document.querySelector('#p1button');
 const p2button = document.querySelector('#p2button');
 const setbtn = document.querySelector('#setBtn');
 const scoreField = document.querySelector('#winningScore');
+const winner = document.querySelector('#winner');
+const p1display = document.querySelector("#p1display");
+const p2display = document.querySelector("#p2display");
 
 let p1score = 0, p2score = 0;
 let winningScore;
@@ -13,21 +16,21 @@ setbtn.addEventListener('click', function(){
 
 p1button.addEventListener('click', function (){
     p1score++; 
-    document.querySelector("#p1display").innerHTML = p1score;
+    p1display.innerHTML = p1score;
     if(p1score === winningScore){
         p1button.disabled = true;
         p2button.disabled = true;
-        document.querySelector('#winner').innerHTML = "Player 1 Wins";
+        winner.innerHTML = "Player 1 Wins";
     }
 })
 
 p2button.addEventListener('click', function (){
     p2score++;
-    document.querySelector("#p2display").innerHTML = p2score;
+    p2display.innerHTML = p2score;
     if(p2score === winningScore){
         p1button.disabled = true;
         p2button.disabled = true;
-        document.querySelector('#winner').innerHTML = "Player 2 Wins";
+        winner.innerHTML = "Player 2 Wins";
     }
 })
 
@@ -35,12 +38,12 @@ p2button.addEventListener('click', function (){
 reset.addEventListener('click', function (){
     p1score = 0;
     p2score = 0;
-    document.querySelector("#p2display").innerHTML = 0;
-    document.querySelector("#p1display").innerHTML = 0;
+    p2display.innerHTML = 0;
+    p1display.innerHTML = 0;
     p1button.disabled = false;
     p2button.disabled = false;
     scoreField.value = null;
-    document.querySelector('#winner').innerHTML = "";
+    winner.innerHTML = "";
 })
 
 
